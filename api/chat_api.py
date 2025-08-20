9e6890a2ef2eb77f7ace2c42afdc8982729b1209
Wikibuda <gusluna@icloud.com>
2025-08-20 08:39:12 -0600

    Error de escritura

--- api/chat_api.py
+++ api/chat_api.py
@@ -46,6 +46,7 @@ CORS(app, resources={
             "https://www.masamadremonterrey.com",
             "https://account.masamadremonterrey.com",
             "http://localhost:8080",
+            "*",
             "http://127.0.0.1:8080",
             "file://"
         ]
